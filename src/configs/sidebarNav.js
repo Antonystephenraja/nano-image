7d4e4119008e8b9f3d2dfcb72e58bbaa978a9719
Aravinthan-cmd <aravinthan.m85@gmail.com>
2024-02-20 17:43:16 +0530

    initial commit

---
+++ src/configs/sidebarNav.js
@@ -0,0 +1,34 @@
+const sidebarNav = [
+    {
+        link: '/',
+        section: 'dashboard',
+        icon: <i className='bx bx-home-alt'></i>,
+        text: 'Dashboard'
+    },
+    {
+        link: '/nano',
+        section: 'nano',
+        icon: <i className='bx bx-line-chart'></i>,
+        text: 'Nano precise'
+    },
+    {
+        link: '/imageTemp',
+        section: 'imageTemperature',
+        icon: <i className='bx bx-line-chart'></i>,
+        text: 'Image'
+    },
+    {
+        link: '/reports',
+        section: 'reports',
+        icon: <i className='bx bx-cube'></i>,
+        text: 'Reports'
+    },
+    {
+        link: '/settings',
+        section: 'settings',
+        icon: <i className='bx bx-cog'></i>,
+        text: 'Settings'
+    }
+]
+
+export default sidebarNav
